8158e0ca3e27f1e1cb1946ea94641a2d141da2f1
Vlad Maraev <vlad@maraev.me>
2017-06-18 15:56:15 +0200

    new story

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-	return 'Yo, it is working!'
+    return 'Yo, it is working!'
 
+@app.route('/new')
+def new_story():
+    return 'here is the story'
+
+    
 if __name__ == "__main__":
 	app.run()
 
